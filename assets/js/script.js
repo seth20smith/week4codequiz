8069f3428d40fca8140d69deb45959ce35c44320
Smith1 <seth20smith@gmail.com>
2022-03-12 19:27:16 -0800

    timer working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,38 @@
-let scoreText = document.querySelector('#Score');
-let score;
+let scoreText = document.querySelector('#username');
+
 // let timer = document.querySelector('.timer')
 // let time = localStorage.getItem('time')
-// let scores = []
+let scores = []
+let score = localStorage.getItem('time')
 
-
-// StartGame = () => {
-//     time = 60
-//     window.location = 'http://127.0.0.1:5500/week4codequiz/1stQuestion.html'
-//     setInterval(function () {
-//         time--
-//         timer.innerHTML = time
-//         console.log(time)
-//         localStorage.setItem('time', time)
-//         score = time
-//         if (window.location == 'file:///Users/Smith1/Desktop/projects/Code%20Quiz%20Week%204/week4codequiz/highScore.html' || window.location == 'http://127.0.0.1:5500/week4codequiz/highScore.html'){
-//             clearInterval()
-//         }
-//     }, 1000);
-// }
 
 storeScore = () => {
-scores.push(document.getElementById('username')+': '+score)
-localStorage.setItem(JSON.stringify('highscores'), scores)
+    let highscores = JSON.parse(localStorage.getItem('highscores'))
+    console.log(scoreText.value)
+scores.push(scoreText.value+': '+score)
+highscores.push(scoreText.value+': '+score)
+localStorage.setItem(('highscores'), JSON.stringify(highscores))
 }
 
 if (window.location == 'http://127.0.0.1:5500/week4codequiz/HighestScores.html'){
-    if(localStorage.getItem('highscores')){
+    // if(JSON.parse(localStorage.getItem('highscores')).length > 0){
+        console.log(JSON.parse(localStorage.getItem('highscores')).length > 0)
         let highscores = JSON.parse(localStorage.getItem('highscores'))
-    let highScoreList = document.querySelector('.highscore')
-    
+    let highScoreList = document.querySelector('.highscores')
+    console.log(highscores)
     for (let i = 0; i < highscores.length; i++) {
-        highScoreList.appendChild(highscores[i]);
+        console.log(highscores[i])
+        highScoreList.innerHTML = `
+       
+        <li>${highscores[i]}</li>
+        <li>${highscores[i-1]}</li>
+        <li>${highscores[i-2]}</li>
+ 
+        `;
         
     }
-    }
+    } else {document.querySelector('#final-score').innerHTML = score}
+
     
-}
+
+    // }
